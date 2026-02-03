72c544295c942f48b017aafdb7d999b0b8c77cf9
PitaoC <christian.pitao07@gmail.com>
2026-02-04 00:26:06 +0800

    add navbar blog

---
+++ flutter_assesment/lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+class BlogApp extends StatelessWidget {
+  const BlogApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Blog',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const HomeScreen(),
+      },
+    );
+  }
+}
--- flutter_assesment/test/widget_test.dart
+++ flutter_assesment/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_assesment/main.dart';
+import 'package:flutter_assesment/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BlogApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
